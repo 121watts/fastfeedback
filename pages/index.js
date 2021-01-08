91e025a88e07d636c5ebdf37247277afc35334e9
Andrew Watkins <andrew.watkinz@gmail.com>
2021-01-08 10:18:46 -0800

    chore: title change

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Demo Day Next!!!
+          HAI APP TEAM BEER
         </h1>
 
         <p className={styles.description}>
